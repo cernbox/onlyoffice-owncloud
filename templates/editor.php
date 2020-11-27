c82eaab8d2907a281fc82b64ceb6b7cd5f91e1ab
Diogo Castro <diogo.castro@cern.ch>
2020-11-27 09:45:17 +0000

    Special chars in file url

--- templates/editor.php
+++ templates/editor.php
@@ -1,7 +1,7 @@
 <script>
      var this_app = '<?php p($_['appName']) ?>';
      var stop_heartbeat = true;
-     var open_file = '<?php p($_['file']) ?>';
+     var open_file = '<?php echo $_['file'] ?>';
      var open_file_type = '<?php p($_['type']) ?>';
 <?php if ($_['pl_token'] != null) { ?>
      var pl_token = '<?php p($_['pl_token']) ?>';
