89d25cc5940916331af87c9c2b0946ef8456a8c2
Diogo Castro <diogo.castro@cern.ch>
2021-05-12 15:56:02 +0200

    Disable OO popup

--- js/main.js
+++ js/main.js
@@ -330,58 +330,58 @@ $(document).ready(function() {
         }); 
 
         // Show pop up with info
-        var current_office = "onlyoffice";
-        $.getJSON(OC.generateUrl('/apps/office'), function(response) {
-            if (response.engine != current_office) {
+        // var current_office = "onlyoffice";
+        // $.getJSON(OC.generateUrl('/apps/office'), function(response) {
+        //     if (response.engine != current_office) {
 
-                var img_url = OC.imagePath(OCA.Onlyoffice.AppName, 'onlyoffice_logo.png');
+        //         var img_url = OC.imagePath(OCA.Onlyoffice.AppName, 'onlyoffice_logo.png');
 
-                $("body").append('<div id="dialog_office">\
-                    <h2>OnlyOffice is the new default office editor!</h2>\
-                    <div style="background: #2b5880;border-radius: 5px 5px 0 0;height: 60px;margin-top: 21px;display: flex;">\
-                        <img src="' + img_url + '" style="max-height:50px;margin: auto;text-align: center;">\
-                    </div>\
-                    <div style="border-radius: 0 0 5px 5px;padding:15px;border:1px solid #ccc;border-top: 0;text-align:left;">\
-                        <ul>\
-                            <li>• Compatibility with Microsoft Office documents</li>\
-                            <li>• Concurrent editing by multiple people</li>\
-                            <li>• Complete set of formatting tools</li>\
-                        </ul>\
-                    </div>\
-                    <p style="margin-top: 9px; font-size: 0.85em; text-align: left; margin-left: 10px;">\
-                        <a href="https://cern.service-now.com/service-portal?id=kb_article&n=KB0006643" style="color: #757575;" target="_blank">Access to MS Office Online is still possible (KB0006643)</a>\
-                    </p>\
-                </div>');
+        //         $("body").append('<div id="dialog_office">\
+        //             <h2>OnlyOffice is the new default office editor!</h2>\
+        //             <div style="background: #2b5880;border-radius: 5px 5px 0 0;height: 60px;margin-top: 21px;display: flex;">\
+        //                 <img src="' + img_url + '" style="max-height:50px;margin: auto;text-align: center;">\
+        //             </div>\
+        //             <div style="border-radius: 0 0 5px 5px;padding:15px;border:1px solid #ccc;border-top: 0;text-align:left;">\
+        //                 <ul>\
+        //                     <li>• Compatibility with Microsoft Office documents</li>\
+        //                     <li>• Concurrent editing by multiple people</li>\
+        //                     <li>• Complete set of formatting tools</li>\
+        //                 </ul>\
+        //             </div>\
+        //             <p style="margin-top: 9px; font-size: 0.85em; text-align: left; margin-left: 10px;">\
+        //                 <a href="https://cern.service-now.com/service-portal?id=kb_article&n=KB0006643" style="color: #757575;" target="_blank">Access to MS Office Online is still possible (KB0006643)</a>\
+        //             </p>\
+        //         </div>');
 
-                $( "#dialog_office" ).dialog({
-                    resizable: false,
-                    height: "auto",
-                    width: 500,
-                    modal: true
-                    })
-                    .on('dialogclose', function(event) {
-                        $.post(OC.generateUrl('/apps/office'), {engine: current_office});
-                    })
-                    .css("padding", "20px");
+        //         $( "#dialog_office" ).dialog({
+        //             resizable: false,
+        //             height: "auto",
+        //             width: 500,
+        //             modal: true
+        //             })
+        //             .on('dialogclose', function(event) {
+        //                 $.post(OC.generateUrl('/apps/office'), {engine: current_office});
+        //             })
+        //             .css("padding", "20px");
 
 
-                $(".ui-widget-overlay.ui-front").css("background", "rgba(0,0,0, 0.65)").css("z-index", "10000");
-                var box_shadow = "0px 0px 45px 0 rgba(0,0,0,0.2)";
-                $(".ui-dialog")
-                    .css("z-index", "10001")
-                    .css("background", "white")
-                    .css("border", "0")
-                    .css("text-align", "center")
-                    .css("box-shadow", box_shadow)
-                    .css("-webkit-box-shadow", box_shadow)
-                    .css("-moz-box-shadow", box_shadow);;
-                $(".ui-dialog-titlebar")
-                    .css("background", "none")
-                    .css("border", "0")
-                    .css("position", "absolute")
-                    .css("right", "3px")
-                    .css("z-index", "1");
-                }
-            });
-        }
-    });
+        //         $(".ui-widget-overlay.ui-front").css("background", "rgba(0,0,0, 0.65)").css("z-index", "10000");
+        //         var box_shadow = "0px 0px 45px 0 rgba(0,0,0,0.2)";
+        //         $(".ui-dialog")
+        //             .css("z-index", "10001")
+        //             .css("background", "white")
+        //             .css("border", "0")
+        //             .css("text-align", "center")
+        //             .css("box-shadow", box_shadow)
+        //             .css("-webkit-box-shadow", box_shadow)
+        //             .css("-moz-box-shadow", box_shadow);;
+        //         $(".ui-dialog-titlebar")
+        //             .css("background", "none")
+        //             .css("border", "0")
+        //             .css("position", "absolute")
+        //             .css("right", "3px")
+        //             .css("z-index", "1");
+        //     }
+        // });
+    }
+});
